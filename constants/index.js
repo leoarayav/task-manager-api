24a30f909f1405e77d989e419119ace0345d38db
Leo Araya <101936353+leoarayav@users.noreply.github.com>
2024-02-21 20:56:46 -0300

    [chore] Just simple constants (not used)

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+/**
+ * @description This file is used to export all the constants used in the application
+ */
+module.exports = {
+  roles: {
+    USER: 0,
+    ADMIN: 1,
+  },
+  tasks: {
+    PENDING: 'PENDING',
+    IN_PROGRESS: 'IN_PROGRESS',
+    COMPLETED: 'COMPLETED',
+  },
+};
