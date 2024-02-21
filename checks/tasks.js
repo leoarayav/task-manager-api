77cc64bd6854e397098a3373b8f8325039d4f2cc
Leo Araya <101936353+leoarayav@users.noreply.github.com>
2024-02-21 20:49:49 -0300

    [feature] Task and User model validation check

---
+++ checks/tasks.js
@@ -0,0 +1,35 @@
+const { check } = require('express-validator');
+
+module.exports = {
+  create: [
+    check('title')
+      .isLength({ min: 3, max: 50 })
+      .withMessage('Title must be between 3 and 50 characters long'),
+    check('description')
+      .isLength({ min: 3, max: 200 })
+      .withMessage('Description must be between 3 and 200 characters long'),
+    check('status')
+      .isIn(['todo', 'in_progress', 'completed'])
+      .withMessage('Status must be todo, in_progress or completed'),
+    check('priority')
+      .isIn(['low', 'medium', 'high'])
+      .withMessage('Priority must be low, medium or high'),
+    check('author').isMongoId().withMessage('Author must be a valid user id'),
+  ],
+  delete: [check('id').isMongoId().withMessage('Task id must be a valid id')],
+  update: [
+    check('id').isMongoId().withMessage('Task id must be a valid id'),
+    check('title')
+      .isLength({ min: 3, max: 50 })
+      .withMessage('Title must be between 3 and 50 characters long'),
+    check('description')
+      .isLength({ min: 3, max: 200 })
+      .withMessage('Description must be between 3 and 200 characters long'),
+    check('status')
+      .isIn(['todo', 'in_progress', 'completed'])
+      .withMessage('Status must be todo, in_progress or completed'),
+    check('priority')
+      .isIn(['low', 'medium', 'high'])
+      .withMessage('Priority must be low, medium or high'),
+  ],
+};
---
+++ checks/users.js
@@ -0,0 +1,29 @@
+const { check } = require('express-validator');
+
+module.exports = {
+  create: [
+    check('username')
+      .isLength({ min: 3, max: 20 })
+      .withMessage('Username must be between 3 and 20 characters long'),
+    check('email').isEmail().withMessage('Email is invalid'),
+    check('password')
+      .isLength({ min: 6, max: 20 })
+      .withMessage('Password must be at least 6 characters long'),
+  ],
+  update: [
+    check('username')
+      .isLength({ min: 3, max: 20 })
+      .withMessage('Username must be between 3 and 20 characters long'),
+    check('email').isEmail().withMessage('Email is invalid'),
+    check('password')
+      .isLength({ min: 6, max: 20 })
+      .withMessage('Password must be at least 6 characters long'),
+  ],
+  delete: [check('id').isMongoId().withMessage('Invalid id provided')],
+  login: [
+    check('email').isEmail().withMessage('Email is invalid'),
+    check('password')
+      .isLength({ min: 6, max: 20 })
+      .withMessage('Password must be at least 6 characters long'),
+  ],
+};
